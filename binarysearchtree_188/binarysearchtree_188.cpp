bc043077d7fc63956df067cb72c3383b29083e82
DeniArif <deniarifwibowo82@gmail.com>
2023-07-03 12:17:54 +0700

    comit

--- binarysearchtree_188/binarysearchtree_188.cpp
+++ binarysearchtree_188/binarysearchtree_188.cpp
@@ -49,3 +49,19 @@ public:
 			parent->rightchild = newNode;//make the right child of the parent point to the new node
 		}
 	}
+
+	void search(string element, Node*& parent, Node*& currentNode) {
+		//this function searches the currentnode of the specified node as well as the current node of its parent
+		currentNode = ROOT;
+		parent == NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
+};
